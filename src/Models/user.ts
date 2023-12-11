5abc653d0b443bf84967112c025d2a91ad965209
Matheus Prates da Luz <matheuslprates@hotmail.com>
2023-12-11 02:28:14 -0300

    Modificações user começo base

--- src/Models/user.ts
+++ src/Models/user.ts
@@ -4,7 +4,24 @@ import { users } from "../Database/users";
  export class User {
     private _id: string
 
-    constructor(private name: string, private email: string, private username: string, private password: string) {
+    constructor(
+        private name: string, 
+        private email: string, 
+        private _username: string, 
+        private password: string
+    ) {
         this._id = randomUUID()
+
+        if (users.find((user) => user.username === this.username)) {
+            throw new Error("already registered username")
+        }
+    }
+
+    get id(): string {
+        return this._id
+    }
+
+    get username(): string {
+        return this._username
     }
 }
